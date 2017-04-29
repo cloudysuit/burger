81c5eb32eb8a635040853e4ccfd423bb2fed84ca
Stephen Hanson <Stephen Hanson>
2017-04-29 00:24:03 -0500

    orm burger homework

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ("El Sapo");
+INSERT INTO burgers (name) VALUES ("El Mundo");
+INSERT INTO burgers (name) VALUES ("La Dama");
+INSERT INTO burgers (name) VALUES ("El Diablito");
+INSERT INTO burgers (name) VALUES ("La Sirena");
+INSERT INTO burgers (name) VALUES ("La Corona");
